26b1644e4a2ba96eb6dd88d3726c6cbb2fa9ebf9
guoliangzhu <zhuguoliang@yunzhangfang.com>
2021-04-10 18:14:02 +0800

    jest 异步测试

--- fetchData.js
+++ fetchData.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 export const fetchDate = (fn) => {
-    axios.get('http://www.dell-lee.com/react/api/demo1.json')
+    axios.get('http://www.dell-lee.com/react/api/demo.json')
     .then((res) => {
         fn(res.data);
     })
+}
+
+export const fetchData = () => {
+    return axios.get('http://www.dell-lee.com/react/api/demo.json');
+}
+
+export const fetchData1 = () => {
+    return axios.get('http://www.dell-lee.com/react/api/demo1.json');
 }
--- fetchData.test.js
+++ fetchData.test.js
@@ -1,10 +1,29 @@
-import { fetchDate } from './fetchData';
+import { fetchDate, fetchData, fetchData1 } from './fetchData';
 
-test('fetch date test', (done) => {
+
+// 回掉类型异步函数的测试 
+test('fetch date callback test', (done) => {
     fetchDate((data) => {
         expect(data).toEqual({
             success: true
         });
         done();
+    })
+})
+
+// promise 异步函数的测试
+test('fetch data promise test', () => {
+    return fetchData().then(res => {
+        expect(res.data).toEqual({
+            success: true
+        })
+    })
+})
+
+test('fetch data 404', () => {
+    // 下面的expect至少执行一次 否则当借口成功的时候expect不执行 测试用例已经是成功
+    expect.assertions(1);
+    return fetchData1().catch(error => {
+        expect(error.toString().indexOf('404') > -1).toBe(true)
     })
 })
