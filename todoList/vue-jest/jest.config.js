5a810d7a8a82c6f969cf87dbe179a7c7122ead4b
guoliangzhu <zhuguoliang@yunzhangfang.com>
2022-08-26 21:42:29 +0800

    test

--- todoList/vue-jest/jest.config.js
+++ todoList/vue-jest/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  testMatch: [
+    '**/tests/unit/**/*.(spec|test).(js|jsx|ts|tsx)|**/__test__/*.(js|jsx|ts|tsx)'
+  ],
 }
---
+++ todoList/vue-jest/tests/unit/HelloWorld.test.js
@@ -0,0 +1,26 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg);
+    
+    // const root = document.createElement('div');
+    // root.className = 'root';
+    // document.body.appendChild(root);
+    // new Vue({
+    //   render: h => h(HelloWorld, {
+    //     props: {
+    //       msg: 'zgl'
+    //     }
+    //   })
+    // }).$mount('.root');
+    console.log(document.body.innerHTML);
+  })
+})
